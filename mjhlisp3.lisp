e1eccb49f563ba298eacb22edc59ba6834eb6f41
Mike <matrixmike@icloud.com>
2021-06-28 22:33:11 +1000

    end of day 1

--- mjhlisp3.lisp
+++ mjhlisp3.lisp
@@ -18,9 +18,12 @@
 (map print (ap colours colours2))
 (times 3 4 )
 (difference  (plus 3 2 ) (plus 4 1))
+
+; define mapc 
 (de mapc (fn l) (cond
   ((null l) nil)
 (T (cons (fn (car l)) (mapc fn (cdr l))))))
+
 (mapc '(lambda (x) (times 2 x)) '(1 3 4 2 0))
 
 ; good down to here
@@ -31,5 +34,7 @@
    (mapc fn (cdr l ))))))
 (setq l '(3 4 5) )
 (mapc '(lambda (n) (plus n 1)) l)
-(map '(lambda (n) (plus n 1) '( 2 3 4)))
+(mapc '(lambda (n) (plus n 1)) '( 2 3 4))
+(map print (mapc '(lambda (n) (plus n 1)) '( 2 3 4))
+)
 
