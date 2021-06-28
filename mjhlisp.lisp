99167d4d91df78c397cefd68b6229327908f36b7
Mike <matrixmike@icloud.com>
2021-06-28 14:19:01 +1000

    lambda seems to work now

--- mjhlisp.lisp
+++ mjhlisp.lisp
@@ -1,7 +1,7 @@
 ; samples to check the emulation from the BCPL cintcode
 ; 27 June 2021
 (assoc 'B '((A . 1)(B . 2) (C . 3)))
-(setq A '(A (LIST) (WITH SUB-LISTS)))
+(setq A '(A (LIST) (WITH SUB-LISTS)))  ; note use of hyphen
 (car A)
 (cdr A)
 (car (cdr A))
---
+++ mjhlisp3.lisp
@@ -0,0 +1,35 @@
+(setq colours '(red white blue))
+(setq colours2 '(cyan magenta yellow))
+
+(plus 3 )
+'colours
+(ap colours colours2)
+(ap colours '( 3 4 5 ))
+
+(setq where (get 'julius 'address))
+(de map (f xs)
+   (cond ((atom xs) xs)
+         (T (cons (f (car xs)) (map f (cdr xs))))
+   )
+)
+
+(map print '(one two three))
+(map print colours)
+(map print (ap colours colours2))
+(times 3 4 )
+(difference  (plus 3 2 ) (plus 4 1))
+(de mapc (fn l) (cond
+  ((null l) nil)
+(T (cons (fn (car l)) (mapc fn (cdr l))))))
+(mapc '(lambda (x) (times 2 x)) '(1 3 4 2 0))
+
+; good down to here
+
+(de mapc (fn l) (cond
+   ((null l) nil)
+(t  (cons (fn (car l))
+   (mapc fn (cdr l ))))))
+(setq l '(3 4 5) )
+(mapc '(lambda (n) (plus n 1)) l)
+(map '(lambda (n) (plus n 1) '( 2 3 4)))
+
